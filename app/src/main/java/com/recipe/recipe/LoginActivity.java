3ac569cfa0ea67a26a2106cfdcd545549bf05c1d
James Brown <jeb572@cs.bham.ac.uk>
2016-07-20 15:33:19 +0100

    Initial commit. Email password login and facebook login

---
+++ app/src/main/java/com/recipe/recipe/LoginActivity.java
@@ -0,0 +1,186 @@
+package com.recipe.recipe;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.facebook.FacebookSdk;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static String TAG = "LOGIN";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText email;
+    private EditText password;
+
+    private LoginButton facebookLoginButton;
+    private CallbackManager mCallbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.etxtEmail);
+        password = (EditText) findViewById(R.id.etxtPassword);
+        facebookLoginButton = (LoginButton) findViewById(R.id.btnLoginWithFacebook);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Tag", "onAuthStateChanged:signed_in" + user.getUid());
+
+                    Intent i = new Intent(getApplicationContext(), TestActivity.class);
+                    startActivity(i);
+
+                } else {
+                    // User is signed out
+                    Log.d("Tag", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        mCallbackManager = CallbackManager.Factory.create();
+        facebookLoginButton.setReadPermissions("email", "public_profile");
+        facebookLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void signUpPressed(View view) {
+
+        Log.d("Tag", "Pressed sign up button");
+
+        if(email.length() == 0 || password.length() == 0) {
+            Toast.makeText(getApplicationContext(), "Email or password is empty", Toast.LENGTH_LONG).show();
+        } else {
+
+            String emailStr = email.getText().toString();
+            String passwordStr = password.getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(emailStr, passwordStr)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d("Tag", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                            // If sign in fails, display a message to the user. If sign in succeeds
+                            // the auth state listener will be notified and logic to handle the
+                            // signed in user can be handled in the listener.
+                            if(!task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Authentication Failed.", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    public void loginPressed(View view) {
+
+        String emailStr = email.getText().toString();
+        String passwordStr = password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(emailStr, passwordStr)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Tag", "signInWithEmailAndPassword:onComplete:" + task.isSuccessful());
+
+                        if(!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Failed to login.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:" + accessToken);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/recipe/recipe/TestActivity.java
@@ -0,0 +1,26 @@
+package com.recipe.recipe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+
+    public void signOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        LoginManager.getInstance().logOut();
+
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+}
