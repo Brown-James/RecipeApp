fda62ce778030f6741a643463473765ddb0243ee
James Brown <jeb572@cs.bham.ac.uk>
2016-08-24 17:44:21 +0100

    Added basic Ingredient class

---
+++ app/src/main/java/com/recipe/recipe/Ingredient.java
@@ -0,0 +1,23 @@
+package com.recipe.recipe;
+
+/**
+ * Created by james on 24/08/16.
+ */
+public class Ingredient {
+
+    private String id;
+    private String name;
+
+    public Ingredient(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
